52358f3c42206c70759949c1d2f30a29e58bffee
cristianA123 <cristianchipan2@gmail.com>
2023-01-26 22:02:07 -0500

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Casa</router-link> |
+    <router-link to="/about">sobre</router-link>
   </nav>
   <router-view/>
 </template>
